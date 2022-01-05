8e155a4bfe273e56a98bb984546553e0424823b7
LeoLeu <63637219+LeoLeuu@users.noreply.github.com>
2022-01-05 16:37:22 +0700

    refactor code

--- src/views/screen/admin/BikeParkInfoScreenHandler.java
+++ src/views/screen/admin/BikeParkInfoScreenHandler.java
@@ -3,12 +3,10 @@ package views.screen.admin;
 import com.google.gson.Gson;
 import entities.BikePark;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import utils.Configs;
 import views.screen.BaseScreenHandler;
-import views.screen.popup.PopupScreen;
 
 import java.io.FileWriter;
 import java.io.IOException;
@@ -45,84 +43,22 @@ public class BikeParkInfoScreenHandler extends BaseScreenHandler {
 
     private BikePark bikePark;
 
+    private List<BikePark> bikeParks;
+
     public BikeParkInfoScreenHandler(Stage stage,String screenPath, BikePark bikePark , List<BikePark> bikeParks, String method) throws IOException {
         super(stage,screenPath);
         this.bikePark = bikePark;
-        int index = bikeParks.indexOf(bikePark);
+        this.bikeParks = bikeParks;
         if (method == "edit") {
-            setInfo();
-            setEditable(true);
-            codeBikePark.setEditable(false);
-            btnAddBikePark.setText("Lưu");
-            btnAddBikePark.setOnMouseClicked(e -> {
-                updateInfoBikePark(bikeParks, index);
-                saveBikeParks(bikeParks);
-                try {
-                    BikeParkManageScreenHandler bikeParkManageScreenHandler = new BikeParkManageScreenHandler(stage, Configs.ADMIN_SCREEN_PATH);
-                    bikeParkManageScreenHandler.show();
-                    PopupScreen.success("Success");
-                } catch (Exception exception){
-                    try {
-                        PopupScreen.error("Error");
-                    } catch (IOException ioException) {
-                        ioException.printStackTrace();
-                    }
-                }
-            });
-        }
-        else if (method == "add"){
-            setEditable(true);
-            codeBikePark.setEditable(true);
-            btnAddBikePark.setText("Thêm bãi xe");
-            btnDeleteBikePark.setVisible(false);
-            btnAddBikePark.setOnMouseClicked(e -> {
-                try{
-                    String newCode = codeBikePark.getText();
-                    BikePark bk = bikeParks.stream().filter(t -> t.getCode().equals(newCode)).findFirst().orElse(null);
-                    if (bk != null){
-                        PopupScreen.error("Bike park code " + codeBikePark.getText() + " already exist");
-                        return;
-                    }
-                    BikePark bikePark1 = new BikePark();
-                    bikePark1.setCode(newCode);
-                    bikePark1.setName(textName.getText());
-                    bikePark1.setNumOfBikes(Integer.parseInt(textEbikes.getText()));
-                    bikePark1.setAddress(textAddress.getText());
-                    bikePark1.setNumOfEBikes(Integer.parseInt(textEbikes.getText()));
-                    bikePark1.setNumOfEmptyDocks(Integer.parseInt(textEmptyDocks.getText()));
-                    bikeParks.add(bikePark1);
-                    saveBikeParks(bikeParks);
-                    BikeParkManageScreenHandler bikeParkManageScreenHandler = new BikeParkManageScreenHandler(stage, Configs.ADMIN_SCREEN_PATH);
-                    bikeParkManageScreenHandler.show();
-                    PopupScreen.success("Success");
-                } catch (Exception exception){
-                    try {
-                        PopupScreen.error("Error");
-                    } catch (IOException ioException) {
-                        ioException.printStackTrace();
-                    }
-                }
-            });
+            int index = bikeParks.indexOf(bikePark);
+            editHandler(index);
+        } else if (method == "add"){
+            addHandler();
         } else {
-            setInfo();
-            setEditable(false);
-            codeBikePark.setEditable(false);
-            btnAddBikePark.setVisible(false);
+            infoHandler();
         }
         btnDeleteBikePark.setOnMouseClicked(event -> {
-            bikeParks.remove(bikePark);
-            saveBikeParks(bikeParks);
-            try {
-                BikeParkManageScreenHandler bikeParkManageScreenHandler = new BikeParkManageScreenHandler(stage, Configs.ADMIN_SCREEN_PATH);
-                bikeParkManageScreenHandler.show();
-                PopupScreen.success("Success");
-            } catch (Exception exception){
-                try {
-                    PopupScreen.error("Error");
-                } catch (IOException ioException) {
-                    ioException.printStackTrace();
-                }
-            }
+            deleteBikePark();
         });
         btnCancel.setOnMouseClicked(event -> {
             try {
@@ -150,7 +86,7 @@ public class BikeParkInfoScreenHandler extends BaseScreenHandler {
         textEmptyDocks.setText(String.valueOf(bikePark.getNumOfEmptyDocks()));
     }
 
-    private void saveBikeParks(List<BikePark> bikeParks){
+    private void saveBikeParks(){
         Gson gson = new Gson();
         try {
             FileWriter fileWriter = new FileWriter("src/entities/data/bikeparks.json");
@@ -161,11 +97,93 @@ public class BikeParkInfoScreenHandler extends BaseScreenHandler {
         }
     }
 
-    private void updateInfoBikePark(List<BikePark> bikeParks, int index){
+    private void updateInfoBikePark(int index){
         bikeParks.get(index).setAddress(textAddress.getText());
         bikeParks.get(index).setName(textName.getText());
         bikeParks.get(index).setNumOfEBikes(Integer.parseInt(textEbikes.getText()));
         bikeParks.get(index).setNumOfBikes(Integer.parseInt(textBikes.getText()));
         bikeParks.get(index).setNumOfEmptyDocks(Integer.parseInt(textEmptyDocks.getText()));
+    }
+
+    private void addBikePark(String newCode){
+        BikePark bikePark = new BikePark();
+        bikePark.setCode(newCode);
+        bikePark.setName(textName.getText());
+        bikePark.setNumOfBikes(Integer.parseInt(textEbikes.getText()));
+        bikePark.setAddress(textAddress.getText());
+        bikePark.setNumOfEBikes(Integer.parseInt(textEbikes.getText()));
+        bikePark.setNumOfEmptyDocks(Integer.parseInt(textEmptyDocks.getText()));
+        bikeParks.add(bikePark);
+        saveBikeParks();
+    }
+
+    private void showPopup(String title, String content){
+        ButtonType buttonTypeCancel = new ButtonType("Đóng", ButtonBar.ButtonData.CANCEL_CLOSE);
+        showAlert(Alert.AlertType.NONE, title, content,buttonTypeCancel );
+    }
+
+    private boolean validateNewBike(String newCode){
+        BikePark bk = bikeParks.stream().filter(t -> t.getCode().equals(newCode)).findFirst().orElse(null);
+        if (bk != null){
+            showPopup("Fail","Bãi xe code " + newCode + " đã tồn tại");
+            return false;
+        }
+        return true;
+    }
+
+    private void editHandler(int index){
+        setInfo();
+        setEditable(true);
+        codeBikePark.setEditable(false);
+        btnAddBikePark.setText("Lưu");
+        btnAddBikePark.setOnMouseClicked(e -> {
+            updateInfoBikePark(index);
+            saveBikeParks();
+            try {
+                BikeParkManageScreenHandler bikeParkManageScreenHandler = new BikeParkManageScreenHandler(stage, Configs.ADMIN_SCREEN_PATH);
+                bikeParkManageScreenHandler.show();
+                showPopup("Success", "Bãi xe đã được chỉnh sửa");
+            } catch (Exception exception){
+                showPopup("Fail", "Unknown error");
+            }
+        });
+    }
+
+    private void addHandler(){
+        setEditable(true);
+        codeBikePark.setEditable(true);
+        btnAddBikePark.setText("Thêm bãi xe");
+        btnDeleteBikePark.setVisible(false);
+        btnAddBikePark.setOnMouseClicked(e -> {
+            try{
+                String newCode = codeBikePark.getText();
+                if (!validateNewBike(newCode)) return;
+                addBikePark(newCode);
+                BikeParkManageScreenHandler bikeParkManageScreenHandler = new BikeParkManageScreenHandler(stage, Configs.ADMIN_SCREEN_PATH);
+                bikeParkManageScreenHandler.show();
+                showPopup("Success","Bãi xe đã được thêm");
+            } catch (Exception exception){
+                showPopup("Fail", "Unknown error");
+            }
+        });
+    }
+
+    private void infoHandler(){
+        setInfo();
+        setEditable(false);
+        codeBikePark.setEditable(false);
+        btnAddBikePark.setVisible(false);
+    }
+
+    private void deleteBikePark(){
+        bikeParks.remove(bikePark);
+        saveBikeParks();
+        try {
+            BikeParkManageScreenHandler bikeParkManageScreenHandler = new BikeParkManageScreenHandler(stage, Configs.ADMIN_SCREEN_PATH);
+            bikeParkManageScreenHandler.show();
+            showPopup("Success", "Bãi xe đã được xóa");
+        } catch (Exception exception){
+            showPopup("Fail", "Unknown error");
+        }
     }
 }
