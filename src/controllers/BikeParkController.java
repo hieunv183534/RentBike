4809622188e7e45dcd27be24e7db951aded6cd5b
LeoLeu <63637219+LeoLeuu@users.noreply.github.com>
2022-01-06 20:44:24 +0700

    fix code

--- src/controllers/BikeParkController.java
+++ src/controllers/BikeParkController.java
@@ -3,6 +3,7 @@ package controllers;
 import entities.BikePark;
 import entities.repository.BikeParkRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BikeParkController extends BaseController{
@@ -10,31 +11,48 @@ public class BikeParkController extends BaseController{
     public BikeParkController(){
     }
 
-    public void editBikePark(List<BikePark> bikeParks, BikePark bikePark, String address, String name, int numOfEBikes, int numOfBikes, int numOfDocks){
+    public void editBikePark(List<BikePark> bikeParks, BikePark bikePark, String address, String name, String numOfEBikes, String numOfBikes,
+                             String numOfDocks){
         int index = bikeParks.indexOf(bikePark);
         bikeParks.get(index).setAddress(address);
         bikeParks.get(index).setName(name);
-        bikeParks.get(index).setNumOfEBikes(numOfEBikes);
-        bikeParks.get(index).setNumOfBikes(numOfBikes);
-        bikeParks.get(index).setNumOfEmptyDocks(numOfDocks);
+        bikeParks.get(index).setNumOfEBikes(Integer.parseInt(numOfEBikes));
+        bikeParks.get(index).setNumOfBikes(Integer.parseInt(numOfBikes));
+        bikeParks.get(index).setNumOfEmptyDocks(Integer.parseInt(numOfDocks));
         new BikeParkRepository().save(bikeParks);
     }
 
-    public void addBikePark(List<BikePark> bikeParks,String code, String address, String name, int numOfEBikes, int numOfBikes, int numOfDocks){
+    public void addBikePark(List<BikePark> bikeParks,String code, String address, String name, String numOfEBikes,
+                            String numOfBikes, String numOfDocks){
+        if (!validateInfoBikePark(numOfEBikes,numOfBikes,numOfDocks))
+            return;
+        if (bikeParks == null) bikeParks = new ArrayList<>();
         BikePark bikePark = new BikePark();
         bikePark.setCode(code);
         bikePark.setAddress(address);
         bikePark.setName(name);
-        bikePark.setNumOfEBikes(numOfEBikes);
-        bikePark.setNumOfBikes(numOfBikes);
-        bikePark.setNumOfEmptyDocks(numOfDocks);
+        bikePark.setNumOfEBikes(Integer.parseInt(numOfEBikes));
+        bikePark.setNumOfBikes(Integer.parseInt(numOfBikes));
+        bikePark.setNumOfEmptyDocks(Integer.parseInt(numOfDocks));
         bikeParks.add(bikePark);
         new BikeParkRepository().save(bikeParks);
     }
 
     public boolean validateNewCode(List<BikePark> bikeParks,String newCode){
+        if (bikeParks == null) return true;
         BikePark bk = bikeParks.stream().filter(t -> t.getCode().equals(newCode)).findFirst().orElse(null);
         if (bk != null){
+            return false;
+        }
+        return true;
+    }
+
+    public boolean validateInfoBikePark(String ebikes, String bikes, String emptyDocks){
+        try {
+            Integer.parseInt(ebikes);
+            Integer.parseInt(bikes);
+            Integer.parseInt(emptyDocks);
+        } catch (Exception e){
             return false;
         }
         return true;
